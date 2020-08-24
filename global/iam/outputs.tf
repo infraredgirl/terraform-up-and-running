c66801096ba024d5097175bd6a60935db19a601e
Ana Krivokapic <ana.krivokapic@elastic.co>
2020-08-25 01:13:39 +0200

    Use for_each instead of count to create IAM users

--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -1,9 +1,8 @@
-output "neo_arn" {
-  value       = aws_iam_user.example[0].arn
-  description = "The ARN for the user Neo"
+output "all_users" {
+  value = aws_iam_user.example
 }
 
 output "all_arns" {
-  value       = aws_iam_user.example[*].arn
+  value       = values(aws_iam_user.example)[*].arn
   description = "The ARNs for all users"
 }
